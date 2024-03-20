17a94e90dbf62b28c572a6682b3412fac9eba086
Ravikanth <s.ravikanth2009@gmail.com>
2024-03-20 19:59:03 +0530

    New code added

---
+++ sample.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "What is your salary..?"
+read salary
+
+echo "What is your expenses..?"
+read expenses
+
+# Check if salary and expenses are equal
+
+if [ $salary == $expenses ];
+ then
+    echo "Salary and Expenses are equal"
+elif [$salary != $expenses];
+ then
+    echo "Salary and Expenses are not equal"
+fi
